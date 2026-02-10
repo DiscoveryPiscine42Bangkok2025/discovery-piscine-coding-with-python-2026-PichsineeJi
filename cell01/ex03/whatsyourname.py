bdc1c3bd47cd896dfd5e48fd6c29e4d5a2a02577
0301Pichsinee Jirawattanakasem <66050301@kmitl.ac.th>
2026-02-10 21:54:36 +0700

    Initial commit

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,6 @@
 first_name = input("Hey, what's your first name? : ")
+x = first_name.strip()
 last_name = input("And your last name? : ")
+y = last_name.strip()
 
 print(f"Well pleased to meet you, {first_name} {last_name}.")
