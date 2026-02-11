0af0063734d7431a91fa48055b9405ffa70a0401
0301Pichsinee Jirawattanakasem <66050301@kmitl.ac.th>
2026-02-12 00:30:09 +0700

    Initial commit

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_array = set()
+for num in array:
+    if num > 5:
+        new_array.add(num + 2)
+
+print(array)
+print(new_array)
