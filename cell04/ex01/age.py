9fe9d8273ad565f81bdebea936b986e6683d6cbf
0301Pichsinee Jirawattanakasem <66050301@kmitl.ac.th>
2026-02-11 19:58:47 +0700

    Initial commit

---
+++ cell04/ex01/age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+age = int(input('Please tell me your age: '))
+print(f'You are currently {age} years old')
+
+i = 1
+while i <= 3:
+    num = 10 * i
+    my_age = age + num
+    print("In ", num, "years, you'll be", my_age, "years old.")
+    i += 1
